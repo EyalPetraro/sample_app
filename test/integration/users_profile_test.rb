00d01b5cc594d1c29c18a0234ab989a968ba9848
Eyal Petraro <eyal.petraro@fiverr.com>
2018-01-04 14:01:26 +0200

    Add user following

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -19,4 +19,23 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
       assert_match micropost.content, response.body
     end
   end
+
+  test "home display" do
+    log_in_as(@user)
+    get root_path
+    assert_response :success
+    assert flash.empty?
+    assert_template 'static_pages/home'
+    assert_select 'title', full_title
+    assert_select 'div.stats', count: 1
+    assert_select "a[href=?]", following_user_path(@user.id)
+    assert_select "a[href=?]", followers_user_path(@user.id)
+    #assert_select 'h1', text: @user.name
+    # assert_select 'h1>img.gravatar'
+    # assert_match @user.microposts.count.to_s, response.body
+    # assert_select 'div.pagination', count: 1
+    # @user.microposts.paginate(page: 1).each do |micropost|
+    #   assert_match micropost.content, response.body
+    # end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -85,4 +85,35 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow a user" do
+    lana = users(:lana)
+    archer = users(:archer)
+    assert_not lana.following?(archer)
+    assert_not lana.followers.include?(archer)
+    lana.follow(archer)
+    assert lana.following?(archer)
+    assert archer.followers.include?(lana)
+    lana.unfollow(archer)
+    assert_not lana.following?(archer)
+    assert_not lana.followers.include?(archer)
+  end
+
+  test "feed should have the right posts" do
+    michael = users(:michael)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
